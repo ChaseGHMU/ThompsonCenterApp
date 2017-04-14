39ef162515afbf3463c671eafa560a597cd718a2
Chase Allen <cjan8c@mail.missouri.edu>
2017-04-13 19:26:01 -0500

    Base StoryBoard added

---
+++ Thompson Center App/Thompson Center App/FormsViewController.swift
@@ -0,0 +1,35 @@
+//
+//  FormsViewController.swift
+//  Thompson Center App
+//
+//  Created by Chase Allen on 4/13/17.
+//  Copyright © 2017 Chase J Allen. All rights reserved.
+//
+
+import UIKit
+
+class FormsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
